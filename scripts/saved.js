76a2a16b1ae38d39e6bad41be176b1832206afd1
NanoAnubis <lubomir_stoykov@abv.bg>
2023-06-17 16:01:41 +0300

    minor changes

--- scripts/saved.js
+++ scripts/saved.js
@@ -7,8 +7,9 @@ document.addEventListener("DOMContentLoaded", function() {
         data.forEach(website => {
           const row = document.createElement("tr");
           row.innerHTML = `
-            <td>${website.url}</td>
+            <td><a href="${website.url}">${website.url}</a></td>
             <td>${website.date}</td>
+            <td><a href="${website.dir}">${website.dir}</a></td>
           `;
           websiteTable.querySelector("tbody").appendChild(row);
         });
