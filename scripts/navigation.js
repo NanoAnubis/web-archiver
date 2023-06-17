b38d98ca34c6d68f89037cf7adda2fb26536b57d
NanoAnubis <lubomir_stoykov@abv.bg>
2023-06-17 18:47:46 +0300

    more info at dropdown

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -25,7 +25,7 @@ fetch('/getwebsites.php')
     data.forEach(website => {
       if(website.url == url){
         const option = document.createElement('option');
-        option.text = website.date;
+        option.text = website.date + ' on mode ' + website.mode;
         option.value = website.dir;
         dropdown.appendChild(option);
       }
